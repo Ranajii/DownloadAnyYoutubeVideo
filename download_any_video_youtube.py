c2558057995451de6e8ea819a54d56960a09bc45
Vishal Kumar Rana <42720439+Ranajii@users.noreply.github.com>
2018-08-27 00:00:01 +0530

    Add files via upload

---
+++ download_any_video_youtube.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import pafy
+
+url = 'https://www.youtube.com/results?search_query='
+x = input('Enter song name: ')
+z = x.replace(' ','+')
+urlnew = url+z
+print(urlnew)
+driver = webdriver.Chrome(r"C:\Users\visha\chromedriver")
+driver.get(urlnew)
+
+video1st = driver.find_element_by_xpath('//*[@id="dismissable"]/div')
+video1st.click()
+
+link = driver.current_url
+video = pafy.new(link)
+best = video.getbest()
+best.resolution, best.extension
+download1 = best.download(filepath="E:\Songs")
+print(x,'is downloaded successfully')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
